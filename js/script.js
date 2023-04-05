b54feff06f7c84f643b93c4aff3d5753939f4a1e
Kauanzera <kauan_falima@hotmail.com>
2023-04-05 13:25:50 -0300

    removido contact me, alterado idioma para en, add algumas experiences.

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*====== typed js ======*/
 const typed = new Typed('.multiple-text', {
-  strings: ['Frontend Developer', 'Streamer', 'Gamer'],
+  strings: ['Web Developer', 'Estudante', 'Gamer'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
